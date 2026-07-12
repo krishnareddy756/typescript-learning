d226a98c05eeab02d2137fd474f26402ba8de9c6
krishnareddy-tech007 <krishna@northpartners.ai>
2026-07-12 17:23:08 +0530

    Covered the Abstract Classes Concept

--- js/classes.js
+++ js/classes.js
@@ -1,3 +1,4 @@
+"use strict";
 // class Empolyee{
 //     empid:number;
 //     empname:string;
@@ -18,16 +19,12 @@
 // let emp1=new Empolyee(1,"krishna reddy");
 // console.log(emp1.empname);
 // console.log(emp1.getDetails());
-
+Object.defineProperty(exports, "__esModule", { value: true });
 // class Mobile{
 //     mobId:number;
-
 //     modelName: string;
-
 //     mobilePrice: number;
-
 //     mobileAvailability: boolean;
-
 //     mobileFeatures:string;
 //     constructor(mobId:number,modelName:string, mobilePrice:number,mobileAvailability:boolean,mobileFeatures:string){
 //         this.mobId=mobId;
@@ -51,7 +48,6 @@
 //         true,
 //         "Qualcomm 625 octa-core"
 //     ),
-
 //     new Mobile(
 //         507,
 //         "Lenovo K6 Note",
@@ -71,7 +67,6 @@
 //     return mobiles.filter(mobile=>mobile.mobilePrice<=price);
 // }
 // console.log(filterByPrice(10000));
-
 // class Product {
 //     private productId: number;
 //     public productName: string;
@@ -85,7 +80,6 @@
 //         return "The productId is " + this.productId;
 //     }
 // }
-
 // let prod1 = new Product(1001, 'Moto G6plus', 'Mobile');
 // // As getProductId() method is implemented inside the class Product thus productId is accessible.
 // console.log(prod1.getProductId());
@@ -93,7 +87,6 @@
 // console.log(prod1.productCategory);
 // This would throw an error as we cannot access private properties outside the class
 // console.log(prod1.productId);
-
 // class Product {
 //     static productName: string = "Mobile"; // Declaring productName property using static keyword
 //     static getProductDetails(): string {
@@ -106,27 +99,21 @@
 // Product.productName = "Tablet";
 // console.log(Product.productName);
 // console.log(Product.getProductDetails());  //Setting value for static property and accessing the same using property name and function name
-
 // class Employee {
 //     name: string;
-
 //     constructor(name: string) {
 //         this.name = name;
 //     }
-
 //     work(): void {
 //         console.log(`Employee is working ${this.name }`);
 //     }
 // }
-
 // class Developer extends Employee {
 //     code(): void {
 //         console.log(`Developer is coding ${this.name}`);
 //     }
 // }
-
 // const dev = new Developer("Krishna");
-
 // console.log(dev.name); // Krishna
 // dev.work();            // Employee is working
 // dev.code();            // Developer is coding
@@ -135,17 +122,13 @@
 //         console.log("Parent constructor");
 //     }
 // }
-
 // class Developer extends Employee {
 //     constructor(name: string, public language: string) {
 //         super(name);
 //         console.log("Child constructor");
 //     }
 // }
-
 // const dev = new Developer("Krishna", "TypeScript");
-
-
 // class Product{
 // 	public productId:number;
 // 	constructor(productId:number){
@@ -165,9 +148,6 @@
 // }
 // var g=new Gadget("Tablet",1234);
 // g.getProduct();
-
-
-
 // abstract class Product {
 //     getCost(amount): void {
 //         console.log("The article costs Rs. " + amount)
@@ -188,105 +168,65 @@
 // console.log(g.getProductName());
 // var c = new Clothing();
 // console.log(c.getProductName());
-
 // abstract class GarageCustomer {
 //     public custName:
-
 //     public registrationNumber
-
 //     public model
-
 //     private make
-
 //     public serviceTax
-
 //     public carWashCharge
 //     abstract  payableAmount():number;
 // }
 // class RegularCustomer extends GarageCustomer{
 //     payableAmount(): number {
 //         Enum baseCharge{Hyundai=6000, Volkswagen=4900, Honda=5000, Toyota=5500};
-
-
 //     }
 // }
-
-abstract class GarageCustomer {
-    public custName: string;
-    public registrationNumber: number;
-    public model: string;
-    private make: string;
-    public serviceTax: number;
-    public carWashCharge: number;
-
-    constructor(
-        model: string,
-        carWashCharge: number,
-        serviceTax: number,
-        registrationNumber: number
-    ) {
+class GarageCustomer {
+    custName;
+    registrationNumber;
+    model;
+    make;
+    serviceTax;
+    carWashCharge;
+    constructor(model, carWashCharge, serviceTax, registrationNumber) {
         this.model = model;
         this.carWashCharge = carWashCharge;
         this.serviceTax = serviceTax;
         this.registrationNumber = registrationNumber;
     }
-
-    abstract payableAmount(): number;
 }
-
-
 class RegularCustomer extends GarageCustomer {
-
-    payableAmount(): number {
-
-        enum baseCharge {
-            Hyundai = 6000,
-            Volkswagen = 4900,
-            Honda = 5000,
-            Toyota = 5500
-        }
-
+    payableAmount() {
+        let baseCharge;
+        (function (baseCharge) {
+            baseCharge[baseCharge["Hyundai"] = 6000] = "Hyundai";
+            baseCharge[baseCharge["Volkswagen"] = 4900] = "Volkswagen";
+            baseCharge[baseCharge["Honda"] = 5000] = "Honda";
+            baseCharge[baseCharge["Toyota"] = 5500] = "Toyota";
+        })(baseCharge || (baseCharge = {}));
         return baseCharge[this.model] +
-               this.carWashCharge +
-               this.serviceTax;
+            this.carWashCharge +
+            this.serviceTax;
     }
 }
-
-
 class PrivilegedCustomer extends GarageCustomer {
-
-    payableAmount(): number {
-
-        enum baseCharge {
-            Hyundai = 5000,
-            Volkswagen = 3900,
-            Honda = 4000,
-            Toyota = 4500
-        }
-
-        let discount: number = this.serviceTax * 10 / 100;
-
+    payableAmount() {
+        let baseCharge;
+        (function (baseCharge) {
+            baseCharge[baseCharge["Hyundai"] = 5000] = "Hyundai";
+            baseCharge[baseCharge["Volkswagen"] = 3900] = "Volkswagen";
+            baseCharge[baseCharge["Honda"] = 4000] = "Honda";
+            baseCharge[baseCharge["Toyota"] = 4500] = "Toyota";
+        })(baseCharge || (baseCharge = {}));
+        let discount = this.serviceTax * 10 / 100;
         return baseCharge[this.model] +
-               this.carWashCharge +
-               (this.serviceTax - discount);
+            this.carWashCharge +
+            (this.serviceTax - discount);
     }
 }
-
-
-let regularCustomer = new RegularCustomer(
-    "Hyundai",
-    100,
-    30,
-    1001
-);
-
-let privilegedCustomer = new PrivilegedCustomer(
-    "Hyundai",
-    100,
-    30,
-    1002
-);
-
+let regularCustomer = new RegularCustomer("Hyundai", 100, 30, 1001);
+let privilegedCustomer = new PrivilegedCustomer("Hyundai", 100, 30, 1002);
 console.log(regularCustomer.payableAmount());
-
 console.log(privilegedCustomer.payableAmount());
+//# sourceMappingURL=classes.js.map
