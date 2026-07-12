c751592a08f7f9daf6448499a917d41210e8d9c7
krishnareddy-tech007 <krishna@northpartners.ai>
2026-07-12 10:20:38 +0530

    All Array Built-in Functions

---
+++ js/arrays.js
@@ -0,0 +1,75 @@
+"use strict";
+// let arr: number[] = [1, 2, 3, 4, 5];
+// let newarr=arr.map((x)=>{
+//     return x*2;
+// });
+// let newarr1=arr.map((x)=>x*3);
+// console.log(newarr);
+// console.log(newarr1);
+Object.defineProperty(exports, "__esModule", { value: true });
+// arr.forEach((x)=>{
+//     console.log(x*4);
+// });
+// let  manufacturers:any[] = [
+//     { "id": "Samsung", "checked": false },
+//     { "id": "Motorola", "checked": false },
+//     { "id": "Apple", "checked": false },
+//     { "id": "Sony", "checked": false }
+// ];
+// console.log("1. Displaying a message using forEach():");
+// function display(){
+//     console.log("Products to display...");
+// }
+// manufacturers.forEach(display);
+// console.log("2. Available Products using: forEach() array function:");
+// manufacturers.forEach( function(manufacturer) {
+//     console.log(manufacturer);
+// });
+// let mfs:string[] = ["Samsung", "Motorola", "Apple", "Sony"];
+// let newmfs:string[]=mfs.map(x=>x.toUpperCase());
+// console.log(newmfs);
+// let filterarr:string[]=mfs.filter((x)=>{
+//     return x.length>=6;
+// });
+// let farr:string[]=mfs.filter(x=>x.length<6);
+// console.log(farr);
+// console.log(filterarr);
+// mfs.forEach((x)=>console.log(x));
+// let nums:number[]=[1,2,3,4,5,6];
+// function filters(num:number):boolean{
+//     return num%2==0;
+// }
+// let arrs:number[]=nums.filter(filters);
+// console.log(arrs);
+// Syntax:-  Array.find(callback)
+// console.log("====== Simple find() example =========");
+// let manufacturers: string[] = ["apple", "sony","motorola","samsung"];
+// function findManufacturer(manufacturer: string) {
+//     if (manufacturer.length > 5) {
+//         return true;
+//     }
+// }
+// let foundManufacturer: string|undefined = manufacturers.find(findManufacturer);
+// console.log(foundManufacturer);
+let arr = [
+    { "id": "sam", "check": true },
+    { "id": "ram", "check": false },
+    { "id": "saam", "check": true },
+    { "id": "raam", "check": false }
+];
+let res = arr
+    .filter(x => x.check == true)
+    .map(x => x.id);
+console.log(res);
+const mobilePriceArray = [25000, 15499, 27500, 18799];
+const incrementedPriceArr = mobilePriceArray.filter(function (number) {
+    return number % 2 !== 0;
+}).map(function (number) {
+    return number + 400;
+});
+console.log(incrementedPriceArr); // => [15899,19199]
+let subjects = ["Mathematics-70", "Science-67", "English-88", "Geography-62", "ComputerSc-55"];
+subjects.map(value => value.toUpperCase()) // converts all the elements to upper case
+    .filter(subjects => parseInt(subjects.substr(-2, 2)) > 65) // filters only the subjects having score greater than 65
+    .forEach(sub => console.log(sub)); // displays all the filtered subjects in console
+//# sourceMappingURL=arrays.js.map
---
+++ js/usman.d.ts
@@ -0,0 +1,2 @@
+export {};
+//# sourceMappingURL=usman.d.ts.map
